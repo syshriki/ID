6ebefc65debaae4b481b9c8ce62e095e778dd84e
Shlomo Shriki <sshriki@industrialdefender.com>
2012-06-12 16:28:50 -0400

    deleted:    load_confgs.pl

--- lib/ID/PatchParser/Windows.pm
+++ lib/ID/PatchParser/Windows.pm
@@ -2,8 +2,10 @@
 package ID::PatchParser::Windows;
 use strict;
 use warnings;
+use File::Basename;
 use base 'ID::PatchParser';
-use ID::Utilities;
+require "load_configs.pl";
+
 #Builds the list of Patchs objects 
 #Arguments: Filename
 #Returns: List of Patches
@@ -14,14 +16,18 @@ sub createPatchObjects{
 	my @patchObjects = ();	
 	my $patch = undef;
 
-	my $parse = new ID::Utilities;
-	my @parsedData = $parse->parsePatchFile($fn);
+	#These lines find the OS of this class (EX: 'ID::PatchParser::Windows' would result in 'Windows')
+	my $class_name = __PACKAGE__;
+	my @class_name_split = split(/:+/,$class_name);
+	$class_name = $class_name_split[-1];
 
-
+	#This does all the work	
+	my @parsedData = load($class_name);
+	print $parsedData[-1][-1];
 	my ($date,$ID,$bulletin_kb,$severity,$impact,$title,$affected_pro,$component_kb,$affected_com,$impact2,$severity2,$suspended,$supercedes,$reboot,$cve,$type,$description) = undef;
 	my @references = ();
 	foreach my $dataRow ( @parsedData ) {
-		$patch = new ID::Patch;
+		$patch = new ID::Patch(ID => "none");
 		$date = shift @$dataRow;
 		$ID = shift @$dataRow;
 		$bulletin_kb = shift @$dataRow;
