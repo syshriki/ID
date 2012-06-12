fdf311ab776fb22e8d3a0c824770610110cacedf
Shlomo Shriki <sshriki@industrialdefender.com>
2012-06-12 14:15:56 -0400

    modified:   load_confgs.pl

--- load_confgs.pl
+++ load_confgs.pl
@@ -1,26 +1,30 @@
 use warnings;
 use strict;
 use Config::IniFiles;
+use ID::Utilities;
 
-my $OS,$method;
+my $OS;
 
 my $cfg = Config::IniFiles->new( -file => "/Configs/bulletins.ini" );
 
+sub load{
+	my $OS = shift;
 
-my $HTMLTables = $cfg->val($OS,"tables");
-my $HTMLheaders_section = $cfg->val($OS,"headers");
-my @HTMLHeaders = qw(eval($HTMLHeaders_section));
-my $HTMLHeadersUrl = $cfg->val($OS,"url") $updateFileURL = $cfg->val($OS,"update")
-r = new ID::Hardware::Vendor(Name => $vendor_name);
+	my $cfg = Config::IniFiles->new( -file => "/Configs/bulletins.ini" );
+	my $file = $cfg->val($OS,"file");
 
-my $file = $cfg($OS,"file");
+	my $HTMLTables = $cfg->val($OS,"tables");
+	my $HTMLheaders_section = $cfg->val($OS,"headers");
+	my @HTMLHeaders = qw(eval($HTMLHeaders_section));
+	my $HTMLHeadersUrl = $cfg->val($OS,"url") $updateFileURL = $cfg->val($OS,"update")
 
-
-if(defined $updateFileURL)){
-	#Update The file
-}
-if(defined $file){
-	#Use file
-}else if(defined @HTMLHeaders && defined $HTMLHeadersURL){
-	#Use HTML
+	if(defined $updateFileURL){
+		&getWebFile("/Bulletins/$file",$updateFileURL);
+	}
+	if(defined $file){
+		return &parsePatchFile($file);
+	}else if(defined @HTMLHeaders && defined $HTMLHeadersURL){
+		return &parseHTMLTable($HTMLHeadersURL,/@htmlhEADERS);
+	}	
+	return "No info found about $OS in Configs/bulletins.ini";
 }
